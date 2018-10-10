50aa653d50907de1c9fe4b029098f31921e4f032
Francis T <francis-t@mm-dhcp-129-181.naist.jp>
2018-10-10 20:33:19 +0900

    [Interim] Initial Work on FHE Crypto Engine

--- crypto_engine/fhe_crypto_engine.py
+++ crypto_engine/fhe_crypto_engine.py
@@ -5,27 +5,62 @@ import struct
 import base64
 import os
 
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
+import seal
+from seal import EncryptionParameters, \
+                 SEALContext, \
+                 KeyGenerator, \
+                 PublicKey, \
+                 SecretKey
 
 class FHECryptoEngine(CryptoEngine):
     def __init__(self):
         CryptoEngine.__init__(self, defs.ENC_MODE_FHE)
         self.log_id = 'FHECryptoEngine'
+        self.encrypt_params = None
+        self.context = None
+
         return
 
     def load_keys(self):
-        # TODO
+        self.private_key = SecretKey()
+        self.private_key.load(defs.FN_FHE_PRIVATE_KEY)
+
+        self.public_key = PublicKey()
+        self.public_key.load(defs.FN_FHE_PUBLIC_KEY)
         
         return True
 
     def generate_keys(self):
-        # TODO
+        if self.encrypt_params == None or \
+           self.context == None:
+            self.init_encrypt_params()
+
+        keygen = KeyGenerator(self.context)
+
+        # Generate the private key
+        self.private_key = keygen.secret_key()
+        self.private_key.save(defs.FN_FHE_PRIVATE_KEY)
+
+        # Generate the public key
+        self.public_key = keygen.public_key()
+        self.public_key.save(defs.FN_FHE_PUBLIC_KEY)
 
         return True
 
+    def init_encrypt_params(self):
+        self.encrypt_params = EncryptionParameters()
+        self.encrypt_params.set_poly_modulus("1x^2048 + 1")
+        self.encrypt_params.set_coeff_modulus(seal.coeff_modulus_128(2048))
+        self.encrypt_params.set_plain_modulus(1 << 8)
+
+        self.context = SEALContext(self.encrypt_params)
+
+        return
+
     def initialize(self, use_old_keys=False):
-        # TODO
+        # Initialize encryption params
+        self.init_encrypt_params()
+
         # Check if the public key file exists
         if os.path.isfile(defs.FN_FHE_PUBLIC_KEY) and \
            os.path.isfile(defs.FN_FHE_PRIVATE_KEY) and \
